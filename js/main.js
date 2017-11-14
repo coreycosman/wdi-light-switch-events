f7f30239175524be3f08e314e5946b7966876769
COREY COSMAN <cosman.corey@gmail.com>
2017-11-14 17:24:00 -0500

    changed some class states, changed some innerHTML

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-
-
+var clickBait = document.querySelector('.switch');
+clickBait.addEventListener('click', function(){
+  // window.alert('#switch-on was clicked');
+  if (clickBait.className === 'switch on') {
+    clickBait.className = 'switch off'
+    var body = document.querySelector("body");
+    body.className = ("dark")
+    var message = document.querySelector(".status")
+    message.innerText = "Hey, who turned the light off?!"
+  }
+  else {
+    clickBait.className = 'switch on'
+    var body = document.querySelector("body");
+    body.className = ("light")
+    var message = document.querySelector(".status")
+    message.innerText = "It's so bright in here!"
+  }
+});
